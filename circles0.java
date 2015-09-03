0ce3cf78acc192251173768cf6b9294908b458a4
reyet70 <reyet70@mail.sunysuffolk.edu>
2015-09-02 20:49:38 -0400

    added ho ho

--- circles0.java
+++ circles0.java
@@ -2,12 +2,12 @@
 
 // Setup:  screen size. //
 void setup() {
-  size(200, 200);                // Try changing the width & height.
+  size(200, 400);                // Try changing the width & height.
 }
 
 // Next frame:  circle at (x,y). //
 void draw() {
-  ellipse( 50,50, 30,30 );       // Try changing the (x,y) and size.
+  ellipse( 60,50, 60,90 );       // Try changing the (x,y) and size.
   
-  text( "ha ha!" , 50, height -20);
+  text( "ho ho!" , 50, height -20);
 }
