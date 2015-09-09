e98a9cf337f9c606d365bab64e2857b6a626dd38
reyet70 <reyet70@mail.sunysuffolk.edu>
2015-09-09 17:01:18 -0400

    Update circles1.java
    Changed width and height

--- circles1.java
+++ circles1.java
@@ -5,16 +5,16 @@ float x,y;
 
 // Setup:  screen size, initialization. //
 void setup() {
-  size(500, 500);        // Try changing the width & height.
-  x=  width/2;
-  y=  height/2;
+  size(150, 300);        // Try changing the width & height.
+  x=  width/10;
+  y=  height/10;
 }
 
 // Next frame:  draw three circles. //
 void draw() {
-  ellipse(x, y, 80, 80);
-  ellipse(x+60, y+60, 40, 40);
-  ellipse(x+90, y+90, 20, 20);
+  ellipse(x, y, 15, 30);
+  ellipse(x+35, y+55, 20, 20);
+  ellipse(x+60, y+60, 10, 10);
  
   // Move them???? //--              x=  x + 2;  y=  y + 1;
   // Insert BREAK here ^
