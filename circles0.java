d6410143ca2fd45f9ba1b2c97431d763bb0d12c6
reyet70 <reyet70@mail.sunysuffolk.edu>
2015-09-09 16:53:36 -0400

    Update circles0.java
    Changed width and height and x,y size

--- circles0.java
+++ circles0.java
@@ -2,10 +2,10 @@
 
 // Setup:  screen size. //
 void setup() {
-  size(200, 200);                // Try changing the width & height.
+  size(75, 135);                // Try changing the width & height.
 }
 
 // Next frame:  circle at (x,y). //
 void draw() {
-  ellipse( 50,50, 30,30 );       // Try changing the (x,y) and size.
+  ellipse(10,80, 25,60 );       // Try changing the (x,y) and size.
 }
