006fdf3583e12d6c6f59da591a286291e9356701
reyet70 <reyet70@mail.sunysuffolk.edu>
2015-09-12 16:04:43 -0400

    Update circles2.java
    Changes in size, fill, background and random

--- circles2.java
+++ circles2.java
@@ -1,7 +1,7 @@
 //// Wild circles #2.
 String title=  "Click for another circle.";
 String subtitle=  "Press any key to erase.";
-String author=  "Bruce Alan Martin";
+String author=  "Ths is difficult";
 
 // GLOBAL VARIABLES //
 float x,y;
@@ -9,19 +9,19 @@ int h,w;
 
 // Setup:  screen size, initialization. //
 void setup() {
-  size(250, 500);
-  x=  width/6;
-  y=  height/4;
-  w=  55;          // width of ellipse.
-  h=  45;          // height of ellipse
+  size(400, 550);
+  x=  width/5;
+  y=  height/3;
+  w=  65;          // width of ellipse.
+  h=  55;          // height of ellipse
 }
 
 // Next frame. //
 void draw() {
-  text( title, 30, 30 );
-  text( subtitle, width/6, 15 );
+  text( title, 65, 45 );
+  text( subtitle, width/10, 19 );
   text( author, 20, height-20 );
-  text( w+"x"+h, width 25,height-25 );
+  text( w+"x"+h, width-25, height-25 );
   //
   ellipse(x, y, w, h);
 }
@@ -37,8 +37,8 @@ void mousePressed() {
 //  Handle keyboard //
 void keyPressed() {
   // Erase everything, change size & color. //
-  background( random(155), random(100), random(155) );
-  fill( random(62), random(90), random(62) );
-  w=  int( random(60,120) );
-  h=  int( random(60,120) );
+  background( random(255), random(100), random(255) );
+  fill( random(58), random(86), random(58) );
+  w=  int( random(70,140) );
+  h=  int( random(70,140) );
 }
